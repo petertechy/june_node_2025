0733f042e8f7483cd761fb566c2cb07e028921c9
petertechy <petertechy01@gmail.com>
2025-07-10 22:49:02 +0100

    Update index.js with recent logic or configuration changes

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const express = require("express")
 const app = express()
 const PORT = 5001
 const ejs = require("ejs")
+const mongoose = require("mongoose")
 app.set("view engine", "ejs")
 //bodyparser - parse(convert) - parseInt, parseFloat
 app.use(express.urlencoded({extended: true}))
 //Templating engine - ejs
+//Uniform Resource Identifier
+//Uniform Resource Locator
+
+const URI = "mongodb+srv://petertechy01:mongodb@my-project.w3cyijl.mongodb.net/student_db?retryWrites=true&w=majority&appName=my-project"
+mongoose.connect(URI)
+.then(()=>{
+    console.log("Mongodb has connected successfully")
+})
+.catch((error)=>{
+    console.log("Mongodb no gree connect")
+    console.log(error)
+})
+
+let studentSchema = mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+})
+
+let studentModel = mongoose.model("student_collection", studentSchema)
+
+
 
 const endPoint = [
     {firstName: "Olanrewaju", lastName: "SQI"},
@@ -44,15 +68,32 @@ app.get("/contact", (req, res)=>{
 })
 
 app.post("/register",(req, res)=>{
-    console.log("User Registered")
-    // res.send("You have registered successfully")
+    // console.log("User Registered")
     console.log(req.body)
-    allUsers.push(req.body)
-    res.redirect("/dashboard")
+    let form = new studentModel(req.body)
+    form.save()
+    .then(()=>{
+        res.redirect("/dashboard")
+        
+    })
+    .catch((err)=>{
+        res.send("An error occurred")
+    })
+    
+    // res.send("You have registered successfully")
+    // allUsers.push(req.body)
 })
 
-app.get("/dashboard", (req, res)=>{
-    res.render("dashboard", {name: "Azeem", gender: "female", allUsers})
+app.get("/dashboard", async (req, res)=>{
+    // res.render("dashboard", {name: "Azeem", gender: "female", allUsers})
+    try{
+        let allUsers = await studentModel.find();
+        res.render("dashboard",{name: "Azeem", gender: "female", allUsers})
+    }
+    catch(error){
+        console.log(error)
+        res.render("Unable to Fetch Users")
+    }
 })
 
 app.listen(PORT, (err)=>{
